a3f2383b5018ae0ddc0eeb22284d262bb3c29a73
Shui <shuiloong@gmail.com>
2017-11-27 21:09:42 +0800

    FINALL RANDOMIZE IMAGES CORRECTLY

--- assets/javascript/vue.js
+++ assets/javascript/vue.js
@@ -1,23 +1,67 @@
-var randomGen = Math.floor((Math.random() * 10) + 1)
 axios.get(`https://swapi.co/api/films/7/`)
   .then(response => {
-      // console.log(response.data.characters)
       characters = response.data.characters
-      // console.log(characters)
       randomChar = characters[Math.floor(Math.random() * characters.length)]
-      console.log(randomChar)
-    })
-  .then(data => {
-  axios.get(`${randomChar}`)
-    .then(response => {
-      current = response.data
   })
   .then(data => {
-  var now = new Vue({
+    axios.get(`${randomChar}`)
+    .then(response => {
+      console.log(characters)
+      current = response.data
+    })
+  .then(data => {
+    var now = new Vue({
     el: '#current',
     data: {
-      current: current
-    }
+      current: current,
+      random: randomChar,
+      images: [
+        { text: 'https://swapi.co/api/people/1/',
+          image: '/assets/images/swcharacters/luke.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/3/',
+          image: '/assets/images/swcharacters/c3po.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/5/',
+          image: '/assets/images/swcharacters/leia.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/13/',
+          image: '/assets/images/swcharacters/chew.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/14/',
+          image: '/assets/images/swcharacters/han.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/27/',
+          image: '/assets/images/swcharacters/ackbar.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/84/',
+          image: '/assets/images/swcharacters/finn.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/85/',
+          image: '/assets/images/swcharacters/rey.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/86/',
+          image: '/assets/images/swcharacters/poe.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/87/',
+          image: '/assets/images/swcharacters/bb8.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        },
+        { text: 'https://swapi.co/api/people/88/',
+          image: '/assets/images/swcharacters/phasma.jpg',
+          background: '/assets/images/swbackgrounds/cliffs.jpg'
+        }
+          ]
+        }
+      })
     })
   })
-})
