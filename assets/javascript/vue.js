8bc43013667131bc563ebf967cc7b0e7e0e818f6
Shui <shuiloong@gmail.com>
2017-11-27 16:17:54 +0800

    added background images

--- assets/javascript/vue.js
+++ assets/javascript/vue.js
@@ -7,34 +7,19 @@ var appInfo = new Vue({
   el: '#appInfo',
   data: {
     infos: data
-  }
-    // data: {
-    //   infos: [
-    //   { name: data.name },
-    //   { text: data.birth_year },
-    //   { text: data.gender },
-    //   { text: data.eye_color },
-    //   { text: data.hair_color },
-    //   { text: data.mass },
-    //   { text: data.skin_color },
-    //   { text: data.homeworld },
-    //   { text: data.url },
-    //   { text: data.vehicles },
-    //   { text: data.films },
-    //   { text: data.starships },
-    // ]
-    // }
+    }
+  })
+  var testing = new Vue({
+  el: '#films',
+  data: {
+    films: data
+    }
   })
 });
-fetch('https://swapi.co/api/species/1/')
-.then(response => response.json())
-.then(data => {
-var appSpecies = new Vue({
-el: '#appSpecies',
+
+var app = new Vue({
+  el: '#app',
   data: {
-    species: [
-    { text: data.name },
-  ]
-  }
+    message: randomGen
+    }
 })
-});
