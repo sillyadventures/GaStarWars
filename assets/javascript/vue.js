076b5b3c58d4cb93072304a22af3c3558e7e4d08
Shui <shuiloong@gmail.com>
2017-11-29 21:42:03 +0800

    fixing still

--- assets/javascript/vue.js
+++ assets/javascript/vue.js
@@ -1,18 +1,18 @@
-var images = ['../images/swbackgrounds/cliffs.jpg',
-              '../images/swbackgrounds/cliffsdawn.jpg',
-              '../images/swbackgrounds/desert.jpg',
-              '../images/swbackgrounds/field.jpg',
-              '../images/swbackgrounds/forest.jpg',
-              '../images/swbackgrounds/meadow.jpg',
-              '../images/swbackgrounds/meadowday.jpg',
-              '../images/swbackgrounds/moon.jpg',
-              '../images/swbackgrounds/mountains.jpg',
-              '../images/swbackgrounds/river.jpg',
-              '../images/swbackgrounds/rocky.jpg',
-              '../images/swbackgrounds/skyline.jpg',
-              '../images/swbackgrounds/skylinesunset.jpg',
-              '../images/swbackgrounds/sunnyfield.jpg',
-              '../images/swbackgrounds/valley.jpg', ];
+var images = ['../assets/images/swbackgrounds/cliffs.jpg',
+              '../assets/images/swbackgrounds/cliffsdawn.jpg',
+              '../assets/images/swbackgrounds/desert.jpg',
+              '../assets/images/swbackgrounds/field.jpg',
+              '../assets/images/swbackgrounds/forest.jpg',
+              '../assets/images/swbackgrounds/meadow.jpg',
+              '../assets/images/swbackgrounds/meadowday.jpg',
+              '../assets/images/swbackgrounds/moon.jpg',
+              '../assets/images/swbackgrounds/mountains.jpg',
+              '../assets/images/swbackgrounds/river.jpg',
+              '../assets/images/swbackgrounds/rocky.jpg',
+              '../assets/images/swbackgrounds/skyline.jpg',
+              '../assets/images/swbackgrounds/skylinesunset.jpg',
+              '../assets/images/swbackgrounds/sunnyfield.jpg',
+              '../assets/images/swbackgrounds/valley.jpg', ];
 $(document).ready(function () {
 $('body').css({'background-image': 'url(' + images[Math.floor(Math.random() *
    images.length)] + ')'});
@@ -34,9 +34,9 @@ axios.get(`https://swapi.co/api/films/7/`)
   .then(response => {
     characters = response.data.characters
     // random character on refresh. using math random
-    // randomChar = characters[Math.floor(Math.random() * characters.length)]
+    randomChar = characters[Math.floor(Math.random() * characters.length)]
     //name for testing, below
-    randomChar = characters[1]
+    // randomChar = characters[1]
   })
   .then(data => {
     axios.get(randomChar)
@@ -61,65 +61,65 @@ axios.get(`https://swapi.co/api/films/7/`)
           starshipsTwo: '',
           images: [
             { text: 'https://swapi.co/api/people/1/',
-              image: '../images/swcharacters/luke.jpg',
-              vehicle: '../images/swvehicles/snowspeeder.jpg',
-              vehicleTwo: '../images/swvehicles/speeder.jpg',
-              starship: '../images/swvehicles/xwing.jpg',
-              starshipTwo: '../images/swvehicles/shuttle.jpg',
-              planet: '../images/swplanets/tattoine.jpg',
-              weapon: '../images/swweapons/lighticon.png'
+              image: '../assets/images/swcharacters/luke.jpg',
+              vehicle: '../assets/images/swvehicles/snowspeeder.jpg',
+              vehicleTwo: '../assets/images/swvehicles/speeder.jpg',
+              starship: '../assets/images/swvehicles/xwing.jpg',
+              starshipTwo: '../assets/images/swvehicles/shuttle.jpg',
+              planet: '../assets/images/swplanets/tattoine.jpg',
+              weapon: '../assets/images/swweapons/lighticon.png'
             },
             { text: 'https://swapi.co/api/people/3/',
-              image: '../images/swcharacters/c3po.jpg',
-              planet: '../images/swplanets/naboo.jpg',
-              weapon: '../images/swweapons/drone.png',
+              image: '../assets/images/swcharacters/c3po.jpg',
+              planet: '../assets/images/swplanets/naboo.jpg',
+              weapon: '../assets/images/swweapons/drone.png',
               friend: '& C-3P)'
             },
             { text: 'https://swapi.co/api/people/5/',
-              image: '../images/swcharacters/leia.jpg',
-              vehicle: '../images/swvehicles/speeder.jpg',
-              planet: '../images/swplanets/alderaan.jpg',
-              weapon: '../images/swweapons/gunTwo.png'
+              image: '../assets/images/swcharacters/leia.jpg',
+              vehicle: '../assets/images/swvehicles/speeder.jpg',
+              planet: '../assets/images/swplanets/alderaan.jpg',
+              weapon: '../assets/images/swweapons/gunTwo.png'
             },
             { text: 'https://swapi.co/api/people/13/',
-              image: '../images/swcharacters/chew.jpg',
-              vehicle: '../images/swvehicles/atst.jpg',
-              starship: '../images/swvehicles/falcon.jpg',
-              starshipTwo: '../images/swvehicles/shuttle.jpg',
-              planet: '../images/swplanets/corellia.jpg',
-              weapon: '../images/swweapons/blaster.png'
+              image: '../assets/images/swcharacters/chew.jpg',
+              vehicle: '../assets/images/swvehicles/atst.jpg',
+              starship: '../assets/images/swvehicles/falcon.jpg',
+              starshipTwo: '../assets/images/swvehicles/shuttle.jpg',
+              planet: '../assets/images/swplanets/corellia.jpg',
+              weapon: '../assets/images/swweapons/blaster.png'
             },
             { text: 'https://swapi.co/api/people/14/',
-              image: '../images/swcharacters/han.jpg',
-              starship: '../images/swvehicles/falcon.jpg',
-              starshipTwo: '../images/swvehicles/shuttle.jpg',
-              planet: '../images/swplanets/moncala.jpg',
-              weapon: '../images/swweapons/gunOne.png'
+              image: '../assets/images/swcharacters/han.jpg',
+              starship: '../assets/images/swvehicles/falcon.jpg',
+              starshipTwo: '../assets/images/swvehicles/shuttle.jpg',
+              planet: '../assets/images/swplanets/moncala.jpg',
+              weapon: '../assets/images/swweapons/gunOne.png'
             },
             { text: 'https://swapi.co/api/people/27/',
-              image: '../images/swcharacters/ackbar.jpg',
-              weapon: '../images/swweapons/blaster.png'
+              image: '../assets/images/swcharacters/ackbar.jpg',
+              weapon: '../assets/images/swweapons/blaster.png'
             },
             { text: 'https://swapi.co/api/people/84/',
-              image: '../images/swcharacters/finn.jpg',
-              weapon: '../images/swweapons/blaster.png'
+              image: '../assets/images/swcharacters/finn.jpg',
+              weapon: '../assets/images/swweapons/blaster.png'
             },
             { text: 'https://swapi.co/api/people/85/',
-              image: '../images/swcharacters/rey.jpeg',
-              weapon: '../images/swweapons/lighticon.png'
+              image: '../assets/images/swcharacters/rey.jpeg',
+              weapon: '../assets/images/swweapons/lighticon.png'
             },
             { text: 'https://swapi.co/api/people/86/',
-              image: '../images/swcharacters/poe.jpg',
-              starship: '../images/swvehicles/xwing.jpg',
-              weapon: '../images/swweapons/starship.png'
+              image: '../assets/images/swcharacters/poe.jpg',
+              starship: '../assets/images/swvehicles/xwing.jpg',
+              weapon: '../assets/images/swweapons/starship.png'
             },
             { text: 'https://swapi.co/api/people/87/',
-              image: '../images/swcharacters/bb8.jpg',
-              weapon: '../images/swweapons/drone.png'
+              image: '../assets/images/swcharacters/bb8.jpg',
+              weapon: '../assets/images/swweapons/drone.png'
             },
             { text: 'https://swapi.co/api/people/88/',
-              image: '../images/swcharacters/phasma.jpg',
-              weapon: '../images/swweapons/blaster.png'
+              image: '../assets/images/swcharacters/phasma.jpg',
+              weapon: '../assets/images/swweapons/blaster.png'
             }
           ]
         }
